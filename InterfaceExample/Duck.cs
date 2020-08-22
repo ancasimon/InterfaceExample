214983ecdc5fb240376a8fc604a4392a62dedc72
Anca Simon <ancarsimon@gmail.com>
2020-08-22 13:40:07 -0500

    Add project files.

---
+++ InterfaceExample/Duck.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace InterfaceExample
+{
+    class Duck: IFlyable
+    {
+        public void Fly()
+        {
+            Console.WriteLine("Quack, I'm a flying duck.");
+        }
+    }
+
+    class Airplane: IFlyable, ITakeOff
+    {
+        public void TakeOff(string destination)
+            {
+            Console.WriteLine($"We're taking off on a plane towards {destination}!");
+        }
+        public void Fly()
+        {
+            Console.WriteLine("We're flying on a plane!");
+        }
+    }
+
+    class Helicopter : IFlyable, ITakeOff
+    {
+        public void TakeOff(string destination)
+        {
+            Console.WriteLine($"We're taking off on a helicopter for {destination}!");
+        }
+        public void Fly()
+        {
+            Console.WriteLine("Hovering in a helicopter.");
+        }
+    }
+
+    interface IFlyable
+    {
+        void Fly();
+    }
+
+    interface ITakeOff
+    {
+        void TakeOff(string destination);
+    }
+
+}
---
+++ InterfaceExample/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace InterfaceExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var duck = new Duck();
+            var plane = new Airplane();
+            var helicopter = new Helicopter();
+
+            //you can build a collection of all the things that implement the IFlyable interface:
+            var flyingThings = new List<IFlyable> { duck, plane, helicopter };
+
+            foreach(var thing in flyingThings)
+            {
+                //pattern matching: check if this thing we are on is using the ITakeOff interface and if yes, put it in the takeOff variable and then let me use it:
+                if (thing is ITakeOff takeOff)
+                {
+                    takeOff.TakeOff("Bahamas");
+                }
+                thing.Fly();
+            }
+        }
+    }
+}
